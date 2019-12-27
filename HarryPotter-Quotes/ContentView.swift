5c866f5af2f8c27fea4c12255fbd5224283ec6df
gianmarcog <marco.giretti@web.de>
2019-12-28 00:38:20 +0100

    Initial Commit

---
+++ HarryPotter-Quotes/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  HarryPotter-Quotes
+//
+//  Created by Gianmarco Giretti on 28.12.19.
+//  Copyright © 2019 Gianmarco Giretti. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
