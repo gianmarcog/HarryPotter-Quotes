05431f10f81a7d00a6246128e70231a51184f799
gianmarcog <marco.giretti@web.de>
2019-12-28 01:50:26 +0100

    Added some default text

--- HarryPotter-Quotes/ContentView.swift
+++ HarryPotter-Quotes/ContentView.swift
@@ -10,7 +10,40 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        Text("Harry Potter Quotes App")
+        MainView()
+    }
+}
+
+struct MainView: View {
+    let quote = ["quote1","quote2","quote1","quote2","quote1","quote2"]
+    
+    var body: some View {
+        VStack {
+            
+            HStack {
+                Text("Harry Potter Quotes App")
+                    .font(.subheadline)
+                    .italic()
+                    .foregroundColor(.orange)
+            }
+            
+            ScrollView(.horizontal, showsIndicators: false) {
+                HStack {
+                    HStack {
+                        ForEach(self.quote, id: \.self) {
+                            quote in
+                            VStack {
+                                Image(systemName: "a.square.fill")
+                                    .resizable()
+                                    .frame(width: 100, height: 100)
+                                
+                                Text(quote)
+                            }
+                        }
+                    }
+                }
+            }
+        }
     }
 }
 
